07c4f3716872d664d1b396ff0de344027e94d8e0
MrCrayfish <mrcrayfish@hotmail.com>
2019-12-14 15:56:33 +1030

    Cultivator can now be applied to Shovels

--- src/main/java/com/mrcrayfish/enchantable/enchantment/CultivatorEnchantment.java
+++ src/main/java/com/mrcrayfish/enchantable/enchantment/CultivatorEnchantment.java
@@ -2,25 +2,28 @@ package com.mrcrayfish.enchantable.enchantment;
 
 import com.mrcrayfish.enchantable.Reference;
 import com.mrcrayfish.enchantable.core.ModEnchantments;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.item.HoeItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUseContext;
+import net.minecraft.item.*;
 import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.player.UseHoeEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.Map;
 
 /**
  * Author: MrCrayfish
@@ -53,41 +56,61 @@ public class CultivatorEnchantment extends Enchantment
     }
 
     @SubscribeEvent
+    public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event)
+    {
+        if(till(event.getWorld(), event.getPos(), event.getFace(), event.getItemStack(), event.getPlayer(), ShovelItem.field_195955_e))
+        {
+            event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
     public static void onUseHoe(UseHoeEvent event)
     {
         ItemUseContext context = event.getContext();
+        if(till(context.getWorld(), context.getPos(), context.getFace(), context.getItem(), context.getPlayer(), HoeItem.HOE_LOOKUP))
+        {
+            event.setResult(Event.Result.ALLOW);
+        }
+    }
 
-        ItemStack stack = context.getItem();
+    private static boolean till(World world, BlockPos pos, Direction face, ItemStack stack, PlayerEntity player, Map<Block, BlockState> replacementMap)
+    {
         if(stack.isEmpty())
-            return;
+            return false;
 
         if(!EnchantmentHelper.getEnchantments(stack).containsKey(ModEnchantments.CULTIVATOR))
-            return;
+            return false;
 
-        World world = context.getWorld();
-        BlockPos pos = context.getPos().add(-1, 0, -1);
-        if(context.getFace() != Direction.DOWN)
+        pos = pos.add(-1, 0, -1);
+        if(face != Direction.DOWN)
         {
             boolean tilled = false;
             for(int i = 0; i < 9; i++)
             {
                 BlockPos groundPos = pos.add(i / 3, 0, i % 3);
-                if(world.isAirBlock(groundPos.up()))
+                boolean air = world.isAirBlock(groundPos.up());
+                boolean replaceable = world.getBlockState(groundPos.up()).getMaterial().isReplaceable();
+                if(air || replaceable)
                 {
-                    BlockState groundState = HoeItem.HOE_LOOKUP.get(world.getBlockState(groundPos).getBlock());
+                    BlockState groundState = replacementMap.get(world.getBlockState(groundPos).getBlock());
                     if(groundState != null)
                     {
                         world.setBlockState(groundPos, groundState, 11);
                         tilled = true;
+                        if(!air)
+                        {
+                            world.setBlockState(groundPos.up(), Blocks.AIR.getDefaultState());
+                        }
                     }
                 }
             }
             if(tilled)
             {
-                PlayerEntity player = context.getPlayer();
                 world.playSound(player, pos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
-                event.setResult(Event.Result.ALLOW);
+                return true;
             }
         }
+        return false;
     }
 }
